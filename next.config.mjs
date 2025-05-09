43ed27f7e4867939a56b1dc4d08e95ab6a1c9c8e
Aniket korwar <aniketkorwa@gmail.com>
2025-05-09 23:22:53 +0530

    Auth using clerk

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+
     reactStrictMode: true,
     webpack(config, { isServer }) {
       if (!isServer) {
